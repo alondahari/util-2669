7f49b0c569f8fb696f53127a41aa8708d3ad3ad7
jazzdragon <climbinghobo@gmail.com>
2018-04-29 10:57:51 -0700

    added blueprint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Intent, Spinner } from "@blueprintjs/core";
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Spinner intent={Intent.PRIMARY} />
     );
   }
 }
