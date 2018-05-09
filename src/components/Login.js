dbdb96d271e01cba4c978c0196b23442269da631
jazzdragon <climbinghobo@gmail.com>
2018-05-08 21:57:05 -0700

    log facebook response

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import FacebookLogin from 'react-facebook-login'
 
+const responseFacebook = (res) => {
+  console.log(res)
+}
+
 export default () =>
   <FacebookLogin
-    appId='1952630821436647'
-    autoLoad
+    appId='1088597931155576'
     fields='name,email,picture'
+    callback={ responseFacebook }
   />
